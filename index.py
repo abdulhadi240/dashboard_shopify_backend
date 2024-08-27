e3a7e56abd338792e202d857ceae93112e3b1052
abdulhadi240 <ah912425@gmail.com>
2024-08-27 21:08:34 +0500

    middleware

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
-import requests
+from fastapi.middleware.cors import CORSMiddleware
+import httpx
 
 app = FastAPI(
     title="Shopify API",
@@ -7,29 +8,35 @@ app = FastAPI(
     version="1.0.0",
     servers=[
         {
-            "url": "https://dashboard-shopify-backend.onrender.com",
+            "url": "https://dashboard-shopify-backend2.onrender.com",
             "description": "Shopify API"
         }
-        
     ]
 )
 
-@app.get("/shopify/orders",response_description="The URL to fetch orders from shopify")
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/orders", response_description="The URL to fetch orders from Shopify")
 async def get_shopify_orders():
-    """Fetches and return shopify order from the specifeid URL
-    This endpoint make a fetch call to the url and get the orders from the shopify which is then converted to json and return as a json"""
-    url = "https://b519f5-ff.myshopify.com/admin/api/2024-07/orders.json"
+    """Fetches and returns Shopify orders from the specified URL.
+    This endpoint makes a fetch call to the URL and retrieves orders from Shopify, which are then converted to JSON and returned."""
+    url = "https://dashboard-shopify-backend.onrender.com/shopify/orders"
     headers = {
-        "X-Shopify-Access-Token": "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",
-        "Content-Type": "application/json",
+        "X-Shopify-Access-Token": "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"
     }
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raises an exception for 4XX/5XX responses
-        return response.json()  # Return the JSON response from Shopify
-    except requests.exceptions.HTTPError as http_err:
-        raise HTTPException(status_code=response.status_code, detail=str(http_err))
-    except Exception as err:
-        raise HTTPException(status_code=500, detail=str(err))
-
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url, headers=headers)
+            response.raise_for_status()  # Raises an exception for 4XX/5XX responses
+            return response.json()  # Return the JSON response from Shopify
+        except httpx.HTTPStatusError as http_err:
+            raise HTTPException(status_code=response.status_code, detail=str(http_err))
+        except Exception as err:
+            raise HTTPException(status_code=500, detail=str(err))
